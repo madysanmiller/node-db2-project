1ed416c17c7ac69ea2fc42dbd8a6972a72a46507
Madysan Miller <madysanmiller2020@gmail.com>
2023-10-04 13:11:38 -0400

    completed

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,19 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+
+  return knex.schema.createTable('cars', table => {
+    table.increments();
+    table.string('vin').notNullable().unique();
+    table.string('make').notNullable();
+    table.string('model').notNullable();
+    table.integer('mileage').unsigned().notNullable();
+    table.string('title').defaultTo('clean');
+    table.string('transmission').defaultTo('automatic');
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+
+  return knex.schema.dropTableIfExists('cars');
 };
--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,53 @@
-// no need to change this file
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+
+
+// module.exports = {
+
+//   development: {
+//     client: 'sqlite3',
+//     connection: {
+//       filename: './dev.sqlite3'
+//     }
+//   },
+
+//   staging: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   },
+
+//   production: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   }
+
+// };
+
 const sharedConfig = {
   client: 'sqlite3',
   useNullAsDefault: true,
